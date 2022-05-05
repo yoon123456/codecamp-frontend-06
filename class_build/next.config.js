eb1d01ec0abff743c077c9fa3dfb29d317b15eda
yoon123456 <yw01124@gmail.com>
2022-05-05 13:23:10 +0900

    dd

--- class_build/next.config.js
+++ class_build/next.config.js
@@ -2,6 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
+  generateBuildId: () => "wonywony",
+  exportPathMap: () => ({
+    "/": { page: "/" },
+    "/boards": { page: "/boards" },
+    "/404": { page: "/404" },
+  }),
 };
 
 module.exports = nextConfig;
+
+//  exportPathMap: () => ({
+//   "/": { page: "/" },
+//   "/boards": { page: "/boards" },
+//   "/404": { page: "/404" },
+// }),  이 부분은 서버사이드렌더링을 시켜주지 않는 부분이다
